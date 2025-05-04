93072ac2b8110c8019b00c65b4d6ccb71e6badf6
binbash <danielbinschmid@outlook.de>
2025-05-04 17:32:53 +0900

    moises db training

---
+++ code/unconditional_moises_db.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+# basic flags
+EXEC_GENERATE_CFG=false
+EXEC_TRAIN=false
+EXEC_TEST=true
+
+# config
+CFG_PATH="./configs/moisesdb/unconditional_guitar_v1.yaml"
+
+function train() {
+    python ./experiments/unconditional_moises_db.py \
+        --mode "train" \
+        --cfg_path $CFG_PATH 
+}
+
+function test() {
+    python ./experiments/unconditional_moises_db.py \
+        --mode "test" \
+        --cfg_path $CFG_PATH
+}
+
+function generate_config() {
+    python ./experiments/unconditional_moises_db.py \
+        --mode generate_config \
+        --cfg_path $CFG_PATH 
+}
+
+if [ "$EXEC_GENERATE_CFG" = true ]; then
+    generate_config
+fi 
+
+if [ "$EXEC_TRAIN" = true ]; then
+    train
+fi 
+
+if [ "$EXEC_TEST" = true ]; then
+    test
+fi 
